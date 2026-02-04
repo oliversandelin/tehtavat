a73c6252ba3b656561eefe289e44e0b4ef802b8e
oliversandelin <oliver.sandelin@gmail.com>
2026-02-04 15:19:21 +0200

    tehtava 1

---
+++ testi.py
@@ -0,0 +1,12 @@
+
+
+pituus =int(input("anna pituutesi"))
+paino = float(input("anna painosi"))
+bmi = paino / (pituus/100)**2
+print("bmi:si on", bmi)
+
+import math
+print(math.ceil(bmi))
+print(math.floor(bmi))
+print(math.pi)
+
