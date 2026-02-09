3918ea4875854819aad503dbff534f16121baaae
oliversandelin <oliver.sandelin@gmail.com>
2026-02-09 19:22:59 +0200

    tehtava 1

---
+++ viikko 1/viikko 1.2.py
@@ -0,0 +1,8 @@
+kanta = float(input("Anna suorakulmion kanta: "))
+korkeus = float(input("Anna suorakulmion korkeus: "))
+
+piiri = 2 * (kanta + korkeus)
+pinta_ala = kanta * korkeus
+
+print(f"Piiri: {piiri}")
+print(f"Pinta-ala: {pinta_ala}")
---
+++ viikko 2/viikko 2.2.py
@@ -0,0 +1,7 @@
+import math
+
+
+sade = float(input("anna ympyrän sade: "))
+pinta_ala = math.pi * (sade**2)
+print(f"pinta-ala on{pinta_ala:.2}")
+
