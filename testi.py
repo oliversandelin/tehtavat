8937f4fc0d0f01feb87c5bc5ead618e763a5b77b
oliversandelin <oliver.sandelin@gmail.com>
2026-02-11 15:07:30 +0200

    tehtava 1

--- testi.py
+++ testi.py
@@ -1,12 +1,15 @@
 
+sukupuoli = input("Oletko M vai N? ")
 
-pituus =int(input("anna pituutesi"))
-paino = float(input("anna painosi"))
-bmi = paino / (pituus/100)**2
-print("bmi:si on", bmi)
+if sukupuoli == "M":
+    hemogloniarvo_m = int(input("Mikä on sinun hemogloniarvo? "))
+    if hemogloniarvo_m < 134: print("alhainen hemoglobiiniarvo")
+    elif 134 <= hemogloniarvo_m <= 195: print("Normaalit hemoglobiiniarvo")
+    elif hemogloniarvo_m > 195: print("Korkea hemoglobiiniarvo")
 
-import math
-print(math.ceil(bmi))
-print(math.floor(bmi))
-print(math.pi)
+if sukupuoli == "N":
+    hemogloniarvo_n = int(input("Mikä on sinun hemogloniarvo? "))
+    if hemogloniarvo_n < 117: print("alhainen hemoglobiiniarvo")
+    elif 117 <= hemogloniarvo_n <= 175: print("Normaalit hemoglobiiniarvo")
+    elif hemogloniarvo_n > 175: print("Korkea hemoglobiiniarvo")
 
--- viikko 3/viikko 3.py
+++ viikko 3/viikko 3.py
@@ -13,4 +13,22 @@ elif hytin_luokka == "C": print("C on ikkunaton hytti autokannen alapuolella.")
 else: print("Virheellinen hyttiluokka")
 
 
+sukupuoli = input("Oletko M vai N? ")
+arvo = int(input("Mikä on sinun hemoglobiiniarvo? "))
+
+if sukupuoli == "M" and arvo < 134: print("alhainen hemoglobiiniarvo")
+elif sukupuoli == "M" and 134 <= arvo <= 195: print("Normaalit hemoglobiiniarvo")
+elif sukupuoli == "M" and arvo > 195: print("Korkea hemoglobiiniarvo")
+
+if sukupuoli == "N" and arvo < 117: print("alhainen hemoglobiiniarvo")
+elif sukupuoli == "N" and 117 <= arvo <= 175: print("Normaalit hemoglobiiniarvo")
+elif sukupuoli == "N" and arvo > 175: print("Korkea hemoglobiiniarvo")
+
+vuosiluku = int(input("anna vuosiluku"))
+if vuosiluku > 0:
+    if (vuosiluku % 4 == 0 and vuosiluku % 100!=0) or (vuosiluku % 400 == 0):
+        print(vuosiluku,"on karkaus vuosi")
+    else:
+        print(vuosiluku,"ei ole karkaus vuosi")
+
 
